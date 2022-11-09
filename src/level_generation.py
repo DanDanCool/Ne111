535e14db5a007c7d8c60c0573a18cff849a6703d
Abaacus <theboss2643@gmail.com>
2022-11-09 17:52:08 -0500

    Update level_generation.py
    Fixed Array Issue, added room visualizer

--- src/level_generation.py
+++ src/level_generation.py
@@ -15,27 +15,27 @@ class level_layout_generator:
         self.yRoomMax = int(self.yMax/3 - self.yRoomMin)
 
     def generate_layout(self):
-        x = [
+        X = [
             0,
             self.xMax/3 + random.randint(-self.xMax/16, self.xMax/16),
             self.xMax*2/3 + random.randint(-self.xMax/16, self.xMax/16),
             self.xMax
         ]
-        y = [
+        Y = [
             0,
             self.yMax/3 + random.randint(-self.yMax/9, self.yMax/9),
             self.yMax*2/3 + random.randint(-self.yMax/9, self.yMax/9),
             self.yMax
         ]
 
-        r = [[-1]*3]*3
+        r = [[None for i in range(3)] for j in range(3)]
 
         for i in range(3):
             for j in range(3):
-                p0 = (int(self.bias_small_rand(x[j], x[j+1]-1)),
-                      int(self.bias_small_rand(y[i], y[i+1]-1)))
-                d = (int(self.bias_small_rand(self.xRoomMin, min(self.xRoomMax, x[j+1] - p0[0]))),
-                     int(self.bias_small_rand(self.yRoomMin, min(self.yRoomMax, y[i+1] - p0[1]))))
+                p0 = (int(self.bias_small_rand(X[j], X[j+1]-self.xRoomMin-1)),
+                      int(self.bias_small_rand(Y[i], Y[i+1]-self.yRoomMin-1)))
+                d = (int(self.bias_small_rand(self.xRoomMin, min(self.xRoomMax, X[j+1] - p0[0]))),
+                     int(self.bias_small_rand(self.yRoomMin, min(self.yRoomMax, Y[i+1] - p0[1]))))
                 r[j][i] = (
                     (p0[0], p0[1]),
                     (p0[0] + d[0], p0[1]),
@@ -43,40 +43,52 @@ class level_layout_generator:
                     (p0[0] + d[0], p0[1] + d[1])
                 )
 
-                print("r:", r[j][i])
-                print(r)
-
-        print()
-        print()
-
-        arr = [[0]*self.yMax]*self.xMax
+        arr = [[0 for i in range(self.yMax)] for j in range(self.xMax)]
         for i in range(3):
             for j in range(3):
-                print(j, i, r[j][i], "--", r[0][i])
                 for p in range(0, 4):
-                    point = r[j][i][p]
-                    print(p, ":", r[j][i][p], "-> 2")
-                    arr[point[0]][point[1]] = 2
+                    point1 = r[j][i][p]
+                    arr[point1[0]][point1[1]] = 1
 
+                    match p:
+                        case 0:
+                            point2 = r[j][i][1]
+                            for x in range(point1[0]+1, point2[0]):
+                                arr[x][point1[1]] = 2
+                            
+                            point2 = r[j][i][2]
+                            for y in range(point1[1]+1, point2[1]):
+                                arr[point1[0]][y] = 3
+                        case 1:
+                            point2 = r[j][i][3]
+                            for y in range(point1[1]+1, point2[1]):
+                                arr[point1[0]][y] = 3
+                        case 2:
+                            point2 = r[j][i][3]
+                            for x in range(point1[0]+1, point2[0]):
+                                arr[x][point1[1]] = 2
         #print("\n\n", arr)
-        # self.debug(arr)
-        # -------
+        self.debug(arr)
 
     def bias_small_rand(self, min, max):
         r = random.random() * random.random()
         return min + (max - min)*r
 
     def debug(self, arr):
+        print("~ "*(self.xMax+2))
         for i in range(self.yMax):
+            print("[", end=" ")
             for j in range(self.xMax):
                 if arr[j][i] == 0:
                     print(" ", end=" ")
-                elif arr[j][i] == 2:
-                    print("|", end=" ")
                 elif arr[j][i] == 1:
+                    print("x", end=" ")
+                elif arr[j][i] == 2:
                     print("-", end=" ")
-            print("")
-
+                elif arr[j][i] == 3:
+                    print("|", end=" ")
+            print("]")
+        print("~ "*(self.xMax+2))
 
 l = level_layout_generator()
 l.generate_layout()
